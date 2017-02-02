4b343a1730b11f84ae9d1b35a9affb5c33b0e878
RohanG0407 <gangaraju.rohan@gmail.com>
2017-02-02 17:44:49 -0500

    2/2 Updated V2
    Logs angle + distance data to .txt file in /logs folder

--- logger.py
+++ logger.py
@@ -1,8 +1,15 @@
 import datetime
+import os
 
 def write_log():
 
 	now = datetime.datetime.now()
+
+	if not os.path.exists('/logs'):
+		print 'Folder not made'
+	else: 
+		print 'Folder made'
+		os.makedirs('logs')	
 
 	hour = now.hour
 	minute = now.minute
@@ -12,14 +19,15 @@ def write_log():
 
 	file = open('logs/{0}.txt'.format(log_name), 'w') 
 
-	file.write("---------------------------")
-	file.write("Raw Angle to Target : %s") %angle_Tangent
-	file write("Raw Distance to Target : %s\n") %actual_Distance
-	file.write("First Turn to Target : %s") %first_Turn
-	file.write("First Distance to Target : %s") %first_Distance
-	file.write("Second Turn to Target : %s") %second_Turn
-	file.write("Second Distance to Target : %s") %second_Turn
+	file.write("---------------------------\n")
+	file.write("Raw Angle to Target : %s \n" %angle_Tangent) 
+	file.write("Raw Distance to Target : %s \n" %actual_Distance) 
+	file.write("First Turn to Target : %s \n" %first_Turn) 
+	file.write("First Distance to Target : %s \n" %first_Distance) 
+	file.write("Second Turn to Target : %s \n"  %second_Turn)
+	file.write("Second Distance to Target : %s \n" %second_Distance) 
 	file.write("------------------------------")
 
 	file.close()
 
+write_log()
